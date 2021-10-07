b5240a0a9cf8becf643af405d420d02d47e45c88
Patrick Wu <Ptrckwu@yahoo.com>
2021-10-07 13:51:24 -0400

    added grid css and pic for welcome page

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -4,8 +4,8 @@ function WelcomePage(){
     return (
         <GodDiv>
             <h1>Hello, Underworld!</h1>
-            <p> Welcome to our attempt at replicating the codex from Hades, the best video game of 2021 </p>
-            <img src="https://static.wikia.nocookie.net/hades_gamepedia_en/images/2/23/CodexScreen.png" alt="The actual codex in the game"/>
+            <h3> Welcome to our attempt at replicating the codex from the 2021 game of the year, Hades</h3>
+            <img src="https://images.squarespace-cdn.com/content/v1/5a67750480bd5e2d33afa823/1605028418107-5AOZ2LSLWBJC460UFIDB/20201110120156_1.jpg" alt="The actual codex in the game"/>
         </GodDiv>
     )
 }
@@ -16,13 +16,14 @@ font-family: ubuntu;
 & img {
     float: left; 
     margin: 20px;
+    width: 100%;
     box-shadow: 10px 10px 5px grey;
 }
   h2 {
-      font-size: 60px;
+      font-size: 50px;
   }  
   h3 {
-    font-size: 30px;
+    font-size: 25px;
 }  
 p {
     font-size: 20px;
