6c771dad857f2a23b27cfe164c1930b98e8e86cd
Matt <aboutmattlaw@gmail.com>
2021-10-07 13:49:46 -0400

    adds Mario to 404

--- src/404.js
+++ src/404.js
@@ -1,8 +1,11 @@
 function NoGod(){
     return (
         <div className="div2">
-            404 God not found
+            <h1>God not found. There is no god.</h1>
+            <img src="https://i.ytimg.com/vi/msb5M3i6FYg/maxresdefault.jpg" width="600px"
+        ></img>
         </div>
     )
 }
 export default NoGod
+
